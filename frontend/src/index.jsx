d185935510a2d574c6002facafbd8f3107e80dc1
Yashwant-3475 <yashwantpatil281@gmail.com>
2026-01-16 09:52:53 +0530

    renaming

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext'; // ✅ added
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* BrowserRouter enables client-side routing in our app */}
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* BrowserRouter enables client-side routing in our app */}
+        <BrowserRouter>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
